c06ca5283cc3dac067dba430fe5f98641a431703
ShailiBoddula <shaili0722@gmail.com>
2025-07-30 15:34:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+import os
+
 app=Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello from Fask"
 if __name__=="__main__":
-    app.run(host="127.0.0.1",port=5000)
+    port = int(os.getenviron.get("PORT",5000))
+    app.run(host="0.0.0.0",port=port)
 
